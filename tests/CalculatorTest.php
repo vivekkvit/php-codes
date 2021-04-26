799404738e8b880e2857dceca842778d26a947d5
vivekkvit <57572074+vivekkvit@users.noreply.github.com>
2021-04-26 16:43:33 +0530

    Update CalculatorTest.php

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -3,7 +3,7 @@
 class CalculatorTest extends \PHPUnit\Framework\TestCase {
     public function testAdd() {
 
-        $calculator = new App\Calculator;
+        $calculator = new app\Calculator;
         $result = $calculator->add(20,5);
 
         $this->assertEquals(25, $result);
@@ -12,7 +12,7 @@ class CalculatorTest extends \PHPUnit\Framework\TestCase {
 
     public function testSubtract() {
 
-        $calculator = new App\Calculator;
+        $calculator = new app\Calculator;
         $result = $calculator->subtract(20,5);
 
         $this->assertEquals(15, $result);
